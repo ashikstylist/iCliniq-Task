7907f6a86b1cf15b161f22cc50d11927d333fe9b
ashikstylist <41476160+ashikstylist@users.noreply.github.com>
2023-02-19 16:24:59 -0800

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,25 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Article from './pages/Article';
 import QnA from './pages/QnA';
 
+const Index = () => {
+  return (
+    <>
+      <h2>
+        use /articles for Articles Page
+      </h2>
+      <h2>
+        use /qa for QnA Page
+      </h2>
+    </>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route exact element={<Index/>} path="/" />
           <Route exact element={<Article/>} path="/articles" />
           <Route exact element={<QnA/>} path="/qa" />
         </Routes>
